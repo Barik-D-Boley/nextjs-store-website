6d6733db9ce5eb8345cb125e4e769e9ef98e88eb
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2022-05-20 15:21:33 -0700

    Did stuff

--- pages/index.js
+++ pages/index.js
@@ -19,16 +19,21 @@ export default function Home() {
             <div className={styles.loginContainer}>
                 <h2>Log in</h2>
                 <form className={styles.loginForm}>
-                    <input className={styles.formInput} type="text" name="email-input" id="email-input" placeholder="Email" />
-                    <input className={styles.formInput} type="password" name="password-input" id="password-input" placeholder="Password" />
+                    <div className={styles.topInputs}>
+                        <input className={styles.formInput} type="text" name="email-input" id="email-input" placeholder="Email" />
+                        <input className={styles.formInput} type="password" name="password-input" id="password-input" placeholder="Password" />
+                    </div>
                     <input className={styles.formInput} type="submit" value="Submit" id={styles.submitBtn} />
                 </form>
-                <Link href="/register">
-                    <a>Register</a>
-                </Link>
-                <Link href="/store">
-                    <a>Continue as Guest</a>
-                </Link>
+
+                <div className={styles.loginLinks}>
+                    <Link href="/register">
+                        <a className={styles.loginLink}>Register</a>
+                    </Link>
+                    <Link href="/store">
+                        <a className={styles.loginLink}>Continue as Guest</a>
+                    </Link>
+                </div>
             </div>
         </div>
     )
--- pages/register/index.js
+++ pages/register/index.js
@@ -3,6 +3,45 @@ import Link from 'next/link';
 import styles from './Register.module.css';
 
 export default function Register() {
+    // Handles the submit event on form submit.
+    const handleSubmit = async (event) => {
+        // Stop the form from submitting and refreshing the page.
+        event.preventDefault()
+
+        // Get data from the form.
+        const data = {
+            name: event.target.name.value,
+            email: event.target.email.value,
+            password: event.target.password.value
+        }
+
+        // Send the data to the server in JSON format.
+        const JSONdata = JSON.stringify(data)
+
+        // API endpoint where we send form data.
+        const endpoint = '/api/user'
+
+        // Form the request for sending data to the server.
+        const options = {
+            // The method is POST because we are sending data.
+            method: 'POST',
+            // Tell the server we're sending JSON.
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            // Body of the request is the JSON data we created above.
+            body: JSONdata,
+        }
+
+        // Send the form data to our forms API on Vercel and get a response.
+        const response = await fetch(endpoint, options)
+
+        // Get the response data from server as JSON.
+        // If server returns the name submitted, that means the form works.
+        const result = await response.json()
+        alert(`Is this your full name: ${result.data}`)
+    }
+
     return (
         <div className={styles.container}>
             <Head>
@@ -11,8 +50,22 @@ export default function Register() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <div>
-                Register
+            <div className={styles.loginContainer}>
+                <h2>Register</h2>
+                <form className={styles.loginForm} onSubmit={handleSubmit}>
+                    <div className={styles.topInputs}>
+                        <input className={styles.formInput} type="name" name="name" id="name" placeholder="Name" required />
+                        <input className={styles.formInput} type="text" name="email" id="email" placeholder="Email" required />
+                        <input className={styles.formInput} type="password" name="password" id="password" placeholder="Password" required />
+                    </div>
+                    <input className={styles.formInput} type="submit" value="Submit" id={styles.submitBtn} />
+                </form>
+
+                <div className={styles.loginLinks}>
+                    <Link href="/">
+                        <a className={styles.loginLink}>Go Home</a>
+                    </Link>
+                </div>
             </div>
         </div>
     )
